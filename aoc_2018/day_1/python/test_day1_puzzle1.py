53c9040fd46bff44567a15d0ef33b02a2f6b8670
girish pasupathy <girishpasupathy@gmail.com>
2020-02-23 08:01:04 +0530

    * Added day3 puzzle part 2 question and added simple input for the puzzle part 2 in a separate file * Refactored tests of day1 and day2 to not duplicate the get_input methods
    Changes to be committed:
    	modified:   aoc_2018/day_1/python/test_day1_puzzle1.py
    	modified:   aoc_2018/day_1/python/test_day1_puzzle2.py
    	modified:   aoc_2018/day_2/python/test_day2_puzzle1.py
    	modified:   aoc_2018/day_2/python/test_day2_puzzle2.py
    	modified:   aoc_2018/day_3/puzzle2_question.md
    	new file:   aoc_2018/day_3/puzzle2_simple_input.txt
    	modified:   aoc_2018/day_3/python/test_day3_puzzle2.py

--- aoc_2018/day_1/python/test_day1_puzzle1.py
+++ aoc_2018/day_1/python/test_day1_puzzle1.py
@@ -5,7 +5,7 @@ from typing import List
 from aoc_2018.day_1.python.day1_puzzle1 import find_aggregate_frequency
 
 
-def _get_input(input_file_name: str) -> List[int]:
+def get_input(input_file_name: str) -> List[int]:
     input_file_path: str = os.path.join(
         Path(os.path.dirname(__file__)).parent, input_file_name
     )
@@ -18,8 +18,8 @@ def _get_input(input_file_name: str) -> List[int]:
 
 
 def test_aggregate_frequency_1() -> None:
-    assert find_aggregate_frequency(_get_input("puzzle1_simple_input.txt")) == 4
+    assert find_aggregate_frequency(get_input("puzzle1_simple_input.txt")) == 4
 
 
 def test_aggregate_frequency_2() -> None:
-    assert find_aggregate_frequency(_get_input("puzzle_input.txt")) == 556
+    assert find_aggregate_frequency(get_input("puzzle_input.txt")) == 556
--- aoc_2018/day_2/python/test_day2_puzzle2.py
+++ aoc_2018/day_2/python/test_day2_puzzle2.py
@@ -3,22 +3,11 @@ from pathlib import Path
 from typing import List
 
 from aoc_2018.day_2.python.day2_puzzle2 import find_common_letters
-
-
-def _get_input(input_file_name: str) -> List[str]:
-    input_file_path: str = os.path.join(
-        Path(os.path.dirname(__file__)).parent, input_file_name
-    )
-    box_ids: List[str] = []
-
-    with open(input_file_path) as input_file:
-        box_ids = [id.strip() for id in input_file]
-
-    return box_ids
+from aoc_2018.day_2.python.test_day2_puzzle1 import get_input
 
 
 def test_find_common_letters_simple() -> None:
-    assert find_common_letters(_get_input("puzzle2_simple_input.txt")) == "fgij"
+    assert find_common_letters(get_input("puzzle2_simple_input.txt")) == "fgij"
 
 
 def test_find_common_letters_corner_case() -> None:
@@ -28,6 +17,6 @@ def test_find_common_letters_corner_case() -> None:
 
 def test_find_common_letters_large():
     assert (
-        find_common_letters(_get_input("puzzle_input.txt"))
+        find_common_letters(get_input("puzzle_input.txt"))
         == "pebjqsalrdnckzfihvtxysomg"
     )
