41a2440746e5a3a7bc7e7a5902ce756d6faadc04
Girish Pasupathy <girip11@users.noreply.github.com>
2020-02-23 08:03:39 +0530

    * Added day3 puzzle part 2 question and added simple input for the puzzle part 2 in a separate file (#4)
    * Refactored tests of day1 and day2 to not duplicate the get_input methods
    
    Changes to be committed:
    	modified:   aoc_2018/day_1/python/test_day1_puzzle1.py
    	modified:   aoc_2018/day_1/python/test_day1_puzzle2.py
    	modified:   aoc_2018/day_2/python/test_day2_puzzle1.py
    	modified:   aoc_2018/day_2/python/test_day2_puzzle2.py
    	modified:   aoc_2018/day_3/puzzle2_question.md
    	new file:   aoc_2018/day_3/puzzle2_simple_input.txt
    	modified:   aoc_2018/day_3/python/test_day3_puzzle2.py

--- aoc_2018/day_1/python/test_day1_puzzle2.py
+++ aoc_2018/day_1/python/test_day1_puzzle2.py
@@ -3,24 +3,12 @@ from pathlib import Path
 from typing import List
 
 from aoc_2018.day_1.python.day1_puzzle2 import find_first_repeating_frequency
-
-
-def _get_input(input_file_name: str) -> List[int]:
-    input_file_path: str = os.path.join(
-        Path(os.path.dirname(__file__)).parent, input_file_name
-    )
-
-    input_frequencies: List[int] = []
-
-    with open(input_file_path) as input_file:
-        input_frequencies = list(map(lambda s: int(s), input_file))
-
-    return input_frequencies
+from aoc_2018.day_1.python.test_day1_puzzle1 import get_input
 
 
 def test_first_repeating_frequency_1() -> None:
-    assert find_first_repeating_frequency(_get_input("puzzle2_simple_input.txt")) == 10
+    assert find_first_repeating_frequency(get_input("puzzle2_simple_input.txt")) == 10
 
 
 def test_first_repeating_frequency_2() -> None:
-    assert find_first_repeating_frequency(_get_input("puzzle_input.txt")) == 448
+    assert find_first_repeating_frequency(get_input("puzzle_input.txt")) == 448
--- aoc_2018/day_3/python/test_day3_puzzle2.py
+++ aoc_2018/day_3/python/test_day3_puzzle2.py
@@ -12,7 +12,7 @@ from aoc_2018.day_3.python.test_day3_puzzle1 import get_input
 
 
 def test_find_non_overlapping_claim_simple() -> None:
-    assert find_non_overlapping_claim(get_input("puzzle1_simple_input.txt")) == "#3"
+    assert find_non_overlapping_claim(get_input("puzzle2_simple_input.txt")) == "#3"
 
 
 def test_find_non_overlapping_claim_complex() -> None:
